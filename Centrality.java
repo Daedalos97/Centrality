dd1c3469ecfe2ff022f00812960c99acceaba3a5
Daedalos97 <sam.j.s.heath@gmail.com>
2016-05-22 21:44:29 +0800

    The two main files for the project
    I have already started on parts of them

---
+++ Centrality.java
@@ -0,0 +1,236 @@
+package org.main;
+
+import java.io.IOException;
+import java.io.FileNotFoundException;
+import static java.lang.Math.pow;
+
+/**
+ * Implements four measures of centrality.
+ * @author Samuel Heath 21725083, Bryan Trac .
+ */
+public class Centrality {
+    
+    private static Graph g;
+    
+    private static void main(String[] args) {
+        Centrality c = new Centrality();
+        g.read(args[0]);
+        g.printEdgeMatrix();
+    }
+    
+    private Centrality() {
+        Graph g = new Graph();
+    }
+    
+    /**
+     * Degree centrality is a measure of how many edges are incident on a
+     * specific node in a graph.
+     * @param g The graph constructed from the input values in the form of an
+     * adjacency matrix.
+     * @return An array of size equal to vertices in the graph. Access via
+     * array[i] where i is the vertex, and the output is the number of vertices
+     * incident on the i th vertex.
+     */
+    public int[] Degree(Graph g) {
+        int[] degree = new int[g.getNumberOfVertices()];
+        int[][] edge = g.getEdgeMatrix(); 
+        for(int i = 0; i<g.getNumberOfVertices();i++)
+            for(int j = 0; j<g.getNumberOfVertices();j++)
+                if(edge[i][j]>0){
+                    degree [i] = degree[i] +1;
+                    degree [j] = degree[j] +1;
+                }
+        int[] max = new int[5];
+        return max;
+    }
+    
+    /**
+     * Private heap implemented priority queue, that is needed to find the 
+     * Minimum Spanning Tree of a graph.
+     */
+    private class MSTQueue{
+        private int[] queue; //the priority queue
+        private int[] key;   // the array of priorities
+        private int[] index; //an index showing where each element is in the priorirty queue
+        private int length; 
+        /**
+         * Constructor that initialises all the elements
+         */
+        public MSTQueue(int x){
+            queue = new int[x];
+            key = new int[x];
+            index = new int[x];
+            for (int i =0; i<key.length;i++){
+                queue[i] = -1;
+                index[i] = -1;
+                key [i] = Integer.MAX_VALUE;
+            }
+            length = 0;
+        }
+        //runs the heapify process to restore the heap property.
+        private void heapify(int qIndex){
+            //percolate the item up the heap
+            int parent = (qIndex-1)/2;
+            while(key[queue[qIndex]]<key[queue[parent]]){
+                int temp = queue[qIndex];
+                queue[qIndex] = queue[parent];
+                queue[parent] = temp;
+                index[queue[qIndex]] = qIndex; 
+                index[temp] = parent;
+                qIndex = parent;
+                parent = (qIndex-1)/2;
+            }
+            //percolate down
+            //find the child with the least key value (ie the highest priority)
+            int minChildIndex = 2*qIndex+1; //left child = 2*qIndex-1
+    
+            if(minChildIndex < length-1 && key[queue[minChildIndex+1]]<key[queue[minChildIndex]]) minChildIndex = minChildIndex+1;
+            
+            //the heapify process. Iteratively swap the element at queueIndex with it's highest 
+            //priority child until the heap property is restored 
+            while(minChildIndex < length && key[queue[minChildIndex]]< key[queue[qIndex]]){
+                int temp = queue[qIndex];
+                queue[qIndex] = queue[minChildIndex];
+                queue[minChildIndex] = temp;
+                index[queue[qIndex]] = qIndex;
+                index[temp] = minChildIndex;
+                qIndex = minChildIndex; 
+                minChildIndex = 2*qIndex+1;
+                if(minChildIndex< length && key[queue[minChildIndex+1]]<key[queue[minChildIndex]]) minChildIndex = minChildIndex+1;
+            }
+        }
+        public boolean isEmpty(){
+            return length ==0;
+        }
+        public Object examine(){
+            if (!isEmpty())
+                return queue[0];
+            else return null;
+        }
+        /**
+         * Inserts the vertex into the queue based on its priority.
+         * If already present in the queue, checks if priority is greater
+         * if so, updates the priority.
+         * 
+         * @param   v   the vertex to be inserted into the queue
+         * @param   p   the priority of vertex v
+         */
+        public void enqueue(int v, int p){
+            if(index[v] ==-1){
+                queue[length]=v;
+                key[v]=p;
+                index[v]=length;
+                length++;
+                heapify(index[v]);
+            }else{
+                if (p< key[v]){
+                    key[v]  = p;
+                    heapify(index[v]);
+                }
+            }
+        }
+        public int dequeue(){
+            if (!isEmpty()){
+                int v = queue[0];
+                index[v] = -1;
+                queue[0] = queue[length-1];
+                index[queue[0]] = 0;
+                length = length -1;
+                heapify(0);
+                return v;
+            }else return -1;
+        }
+        public int getPriority(int x){
+            return key[x];
+        }
+    }
+    /**
+     * Closeness Centrality is a measure of 
+     * @param   g The graph constructed from the input values in the form of an
+     * adjacency matrix.
+     * @return  
+     */
+    public double Close(Graph g, int start) {
+        int[][] edge = g.getEdgeMatrix();
+        boolean[] visited = new boolean[g.getNumberOfVertices()];
+        int[] distances = new int[g.getNumberOfVertices()];
+        for(int init=0; init<g.getNumberOfVertices(); init ++){
+            distances[init] = Integer.MAX_VALUE;
+        }
+        MSTQueue q = new MSTQueue(g.getNumberOfVertices());
+        int index;
+        q.enqueue(start,0);
+        distances[start] = 0;
+        while (!q.isEmpty()){
+            index = q.dequeue();
+            visited[index] = true;
+            for(int endNode = 0; endNode<g.getNumberOfVertices();endNode++){
+                if (edge[index][endNode]>0)
+                    if(!visited[endNode]){
+                        if(((distances[index]+1)<distances[endNode])){
+                            distances[endNode] = (distances[index]+1);
+                            q.enqueue(endNode,distances[index]+1);
+                        }
+                    }
+            }           
+        }
+        double close = 0;
+        for(int find=0; find<g.getNumberOfVertices(); find++)
+            close = close + distances[find];
+        close = 1/close;
+        return close;
+    }
+    
+    /**
+     * Betweenness Centrality measures the importance of a vertex based on the 
+     * sum of the shortest paths that pass through that vertex.
+     * @param  g  The graph constructed from the input values in the form of an
+     * adjacency matrix.
+     * @param  v  The vertex we are measuring the Betweenness Centrality for.
+     * @return 
+     */
+    public int[] Between(Graph g, int v) {
+        
+        return null; // edit return statement.
+    }
+    
+    
+    /**
+     * 
+     * @param  g  The graph we will use this centrality measure on.
+     * @param  start The vertex we will start on.
+     * @param  factor The alpha value that changes each step a vertex is away from the start.
+     * @return 
+     */
+    public double Katz(Graph g, int start, double factor) {
+        int[][] edge = g.getEdgeMatrix();
+        boolean[] visited = new boolean[g.getNumberOfVertices()];
+        int[] distances = new int[g.getNumberOfVertices()];
+        for(int init=0; init<g.getNumberOfVertices(); init ++){
+            distances[init] = Integer.MAX_VALUE;
+        }
+        MSTQueue q = new MSTQueue(g.getNumberOfVertices());
+        int index;
+        q.enqueue(start,0);
+        distances[start] = 0;
+        while (!q.isEmpty()){
+            index = q.dequeue();
+            visited[index] = true;
+            for(int endNode = 0; endNode<g.getNumberOfVertices();endNode++){
+                if (edge[index][endNode]>0)
+                    if(!visited[endNode]){
+                        if(((distances[index]+1)<distances[endNode])){
+                            distances[endNode] = (distances[index]+1);
+                            q.enqueue(endNode,distances[index]+1);
+                        }
+                    }
+            }           
+        }
+        double katz = 0;
+        for(int calc = 0; calc<g.getNumberOfVertices();calc++){
+            if (distances[calc] !=Integer.MAX_VALUE)
+                katz = katz+ Math.pow(factor,distances[calc]);
+        }
+        return katz;
+        }
+    }
---
+++ Graph.java
@@ -0,0 +1,191 @@
+import java.util.Scanner;
+import java.io.IOException;
+import java.io.FileReader;
+import java.io.LineNumberReader;
+import java.io.File;
+
+/**
+ * Implements a class which can read an edge data file and produce an adjacency
+ * matrix.
+ * @author Sam 21725083, Brayn Trac .
+ */
+public class Graph {
+    private int[][] matrix; // The adjacency matrix of the graph from the data supplied.
+    private int[] nodes; // The array of the unique nodes in graph sorted in ascending order.
+    private int nodeNum = 0; // Used to hold the length of vertices in this graph.
+    
+    /**
+     * Reads the edge data from an input file.
+     * @param filename Local path to the file and the extension e.g. ".txt".
+     * @throws IOException If file is not in the local directory.
+     */
+    public void read(String filename) throws IOException {
+        try {
+            File file = new File(Graph.class.getProtectionDomain().getCodeSource().getLocation().getFile());        
+            String path = file.getParent() + File.separator + filename + ".txt";
+            //Find out the max number of nodes in the graph.
+            LineNumberReader  lnr = new LineNumberReader(new FileReader(path));
+            lnr.skip(Long.MAX_VALUE);
+            int lines = lnr.getLineNumber();
+            lnr.close();
+            nodes = new int[(lines*2)]; //Create array with max size
+            Scanner scan = new Scanner(new FileReader(path));
+            while (scan.hasNextLine()) {
+                for (String s : scan.nextLine().split(" "))
+                    addNode(Integer.parseInt(s)); //Check nodes are unique.
+                    
+            }
+            quickSort(nodes, 0, nodeNum-1); //Sort nodes so we can binary search them later.
+            
+            //Create Adjacency matrix from the number of nodes in graph. nodes[i] --> matrix[i][...]
+            //nodes[i] corresponds to i th position in the matrix, so we have nodes[i] corresponds to
+            //the relationship between node i and all the other nodes.
+            matrix = new int[nodeNum][nodeNum];
+            for (int i = 0; i < matrix.length; i++) {
+                for (int j = 0; j < matrix.length; j++) matrix[i][j] = 0;
+            }
+            //Writes edges into the adjacency matrix.
+            scan = new Scanner(new FileReader(path));
+            while (scan.hasNextLine()) {
+                String[] line = scan.nextLine().split(" ");
+                matrix[searchNodes(Integer.parseInt(line[0]))][searchNodes(Integer.parseInt(line[1]))] = 1;
+            }
+        } catch (IOException IOE) {
+            usage();
+            IOE.printStackTrace();
+        }
+    }
+    
+    /**
+     * Outputs the correct usage of the read method, if the method is called and
+     * no file is readable.
+     */
+    private void usage() {
+        System.out.println("Please try again, and enter a filename argument"
+                + " that is in the local directory of this code.");
+    }
+    
+    /**
+     * Adds a node to the node matrix iff it is not already in there.
+     * @param node The (hopefully) unique node being added to the nodes array.
+     */
+    private void addNode(int node) {
+        if (nodeNum == 0) {
+            nodes[nodeNum] = node;
+            nodeNum++;
+        } else if (!Search(nodes, node)) {
+            nodes[nodeNum] = node;
+            nodeNum++;
+        }
+    }
+    
+    /**
+     * Returns to the user the node number, with the same index as the Edge Matrix.
+     * @param   pos The index from the adjacency matrix we want to know the node number of.
+     * @return  The full id/number of the node which corresponds to the index of the Adjacency Matrix.
+     */
+    public int getNode(int pos) {
+        return nodes[pos];
+    }
+    
+    /**
+     * Searches the nodes array to check whether or not a node has been added.
+     * @param a     The array we are looking over.
+     * @param query The node we are checking to see if if it has been added.
+     * @return      The index of searched item or -1 if it is not found.
+     */
+    private boolean Search(int[] a, int query) {
+        for (int i = 0; i < a.length; i ++) {
+            if (a[i] == query) return true;
+        }
+        return false;
+    }
+    
+    /**
+     * Implements a Binary Search of the sorted nodes and returns the index of 
+     * the queried node if it exists.
+     * @param   query The vertex we want to find the 
+     * @return  The index of the queried vertex in the node matrix, iff it exists
+     * other wise return unknown (-1).
+     */
+    public int searchNodes(int query) {
+        int min = 0;
+        int max = nodeNum-1;
+        while (max >= min) {
+            int mid = (min+max)/2;
+            if (nodes[mid] == query) return mid;
+            if (nodes[mid] < query) {
+                min = mid+1;
+            } else if (nodes[mid] > query) {
+                max = mid-1;
+            }
+        }
+        return -1;
+    }
+    
+    /**
+     * The number of vertices in the graph.
+     * @return The number of vertices in the imported graph.
+     */
+    public int getNumberOfVertices() {
+        return this.nodeNum;
+    }
+    
+    /**
+     * Returns the Adjacency Matrix for the graph that has been read.
+     * @return The stored adjacency matrix.
+     */
+    public int[][] getEdgeMatrix() {
+        return this.matrix;
+    }
+    
+    /**
+     * Prints the adjacency matrix.
+     */
+    public void printEdgeMatrix() {
+        for(int i = 0; i < this.matrix.length; i++) {
+            for (int j = 0; j < this.matrix.length; j ++) {
+                System.out.print(this.matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+    
+    /**
+     * 
+     * @param  a  is the array of integers
+     * @param  p  lower bound of the range
+     * @param  r  upper bound of the range
+     * @return i  the pivot point value
+     */
+    private int partition(int[] a, int p, int r) {
+        int x = a[r];
+        int i = p-1;
+        for (int j = p; j < r; j++) {
+            if (a[j] <= x) {
+                i = i + 1;
+                int n = a[i];
+                a[i] = a[j];
+                a[j] = n;
+            }
+        }
+        int n1 = a[i+1];
+        a[i+1] = a[r];
+        a[r] = n1;
+        return i+1;
+    }
+    
+    /**
+     * Implements the recursive part of the quicksort algorithm.
+     * @param a the array of integers
+     * @param p the lower bound of the array
+     * @param r the upper bound of the array
+     */
+    private void quickSort(int[] a, int p, int r) {
+        if (p < r) {
+            int q = partition(a, p, r);
+            quickSort(a, p, q-1);
+            quickSort(a, q+1, r);
+        }
+    }
+}
